59dfcdeda681728730df1cd63020934fd1e4ecb4
Chris Thede <christhede17@gmail.com>
2018-09-17 19:19:11 -0400

    More Exercises

---
+++ delete_if.rb
@@ -0,0 +1,11 @@
+arr = ['snow', 'winter', 'ice', 'slippery', 'salted roads', 'white trees']
+
+# arr.delete_if { |word| word.start_with?("s" || "w") }
+
+arr.delete_if do |word|
+	word.start_with?("s", "w")
+end
+
+
+puts arr
+
